2261bfdfde1dfa5c088b12a55eacd44b41501a21
JackyJH-Zhang <69808531+JackyJH-Zhang@users.noreply.github.com>
2020-08-19 00:40:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,21 @@ import json
 with open('setting.json', mode ='r', encoding='utf8') as jfile:
      jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='')
 
 @bot.event
 async def on_ready():
     print(">>bot is online")
 
 @bot.event
-async def on_member_join(member):
-    print(f'{member}join!')
+async def on_ready():
+    channel = bot.get_channel(539038547232030725)
+    await channel.send('舒服啦')
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(369793161679339532)
+    await channel.send('舒服啦')
 
 @bot.event
 async def on_member_remove(member):
@@ -30,5 +36,21 @@ async def good(ctx):
 @bot.command()
 async def POP(ctx):
     await ctx.send('Hi~小成')
+
+@bot.command()
+async def 可憐(ctx):
+    await ctx.send('我好可憐阿!')
+
+@bot.command()
+async def 蛇丸(ctx):
+    await ctx.send('耶~~~~~~~')
+
+@bot.command()
+async def 哭阿(ctx):
+    await ctx.send('舒服啦~')
+
+@bot.command()
+async def Hi(ctx):
+    await ctx.send('我誰!!我瘋子!')
 
 bot.run(jdata['TOKEN'])
