a1b2bdbd81ca6cc52863b0fdc2ff177211bd89a5
JackyJH-Zhang <69808531+JackyJH-Zhang@users.noreply.github.com>
2020-08-18 22:56:05 +0800

    Day 2

--- bot.py
+++ bot.py
@@ -1,9 +1,34 @@
 import discord
 from discord.ext import commands
+import json
+
+with open('setting.json', mode ='r', encoding='utf8') as jfile:
+     jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix='[')
+
 @bot.event
 async def on_ready():
     print(">>bot is online")
 
-bot.run('NzQ0OTMwNjQ4MDkwNzM4NzMw.XzqYug.uFhN4cOlgy7aToYVPRo8E5jUjoc')
+@bot.event
+async def on_member_join(member):
+    print(f'{member}join!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member}leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(bot.latency)
+
+@bot.command()
+async def good(ctx):
+    await ctx.send('楊賢你好可愛啊!')
+
+@bot.command()
+async def POP(ctx):
+    await ctx.send('Hi~小成')
+
+bot.run(jdata['TOKEN'])
